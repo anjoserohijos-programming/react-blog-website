ecf6a8fe14e8e5dd9a01a957b7e9e5352dbc90fb
anjoserohijos-programming <aserohijos88@gmail.com>
2024-02-06 18:22:12 +0800

    [ADD]: Added a function to handle basic props

---
+++ src/SampleProperties.jsx
@@ -0,0 +1,12 @@
+
+
+
+export default function SampleProperties(props){
+    return (
+        <div className="sampleprop">
+            <h2>
+                {props.name}
+            </h2>
+        </div>
+    );
+}
